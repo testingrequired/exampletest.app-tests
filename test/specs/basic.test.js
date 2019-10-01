851d0d30e8e123eaac34d1ee02b1df4aebd65c3f
Kylee Tilley <kyleetilley@gmail.com>
2019-09-30 21:57:00 -0500

    Add wdio

---
+++ test/specs/basic.test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+describe("Landing Page", () => {
+  it("should have the correct title", async () => {
+    await browser.url("/");
+    const title = await browser.getTitle();
+    assert.strictEqual(title, "Lemon - Home");
+  });
+});
