9f912d0e1607e4e9f1e4c8be72e6826517d3178d
Kylee Tilley <kyleetilley@gmail.com>
2019-10-01 20:49:30 -0500

    Add test for loading a config payload

---
+++ test/specs/load.test.js
@@ -0,0 +1,11 @@
+import assert from "assert";
+
+describe("Load", () => {
+  const payload =
+    "eyJzZWVkIjoxNjQxMzkyODEwLCJjb25maWciOnsibG9hZGluZ0RlbGF5IjowLCJsb2FkaW5nSml0dGVyIjpmYWxzZSwic2hvd0xvYWRpbmdTY3JlZW4iOmZhbHNlLCJ1c2Vyc0Ftb3VudCI6MTAsInBvc3RzQW1vdW50IjoxMDAwfX0=";
+
+  it("should have correct title on shared", async () => {
+    await browser.url(`/_/load?payload=${payload}`);
+    assert.strictEqual(await browser.getUrl(), "https://exampletest.app/");
+  });
+});
